ed517677f16de91726daa5daa3195503ef7f9112
Sergey Alyaev <cobxo3bot@gmail.com>
2017-04-28 21:35:56 +0200

    Added midnight line

--- src/c/map_layer.c
+++ src/c/map_layer.c
@@ -1,6 +1,36 @@
 #include <pebble.h>
 #include "map_layer.h"
 
+struct my_point get_sun_point(int time){
+  float day_of_year; // value from 0 to 1 of progress through a year
+  float time_of_day; // value from 0 to 1 of progress through a day
+  // approx number of leap years since epoch
+  // = now / SECONDS_IN_YEAR * .24; (.24 = average rate of leap years)
+  int leap_years = (int)((float)time / 131487192.0);
+  // day_of_year is an estimate, but should be correct to within one day
+  day_of_year = time - (((int)((float)time / 31556926.0) * 365 + leap_years) * 86400);
+  day_of_year = day_of_year / 86400.0;
+  time_of_day = day_of_year - (int)day_of_year;
+  day_of_year = day_of_year / 365.0;
+  struct my_point sun_point;
+  // ##### calculate the position of the sun
+  // left to right of world goes from 0 to 65536
+  sun_point.x = (int)((float)TRIG_MAX_ANGLE * (1.0 - time_of_day));
+  // bottom to top of world goes from -32768 to 32768
+  // 0.2164 is march 20, the 79th day of the year, the march equinox
+  // Earth's inclination is 23.4 degrees, so sun should vary 23.4/90=.26 up and down
+  sun_point.y = -sin_lookup((day_of_year - 0.2164) * TRIG_MAX_ANGLE) * .26 * .25;
+  
+  return sun_point;
+}
+
+struct my_point get_dark_point(int time){
+  struct my_point dark_point = get_sun_point(time);
+  dark_point.x = (dark_point.x + TRIG_MAX_ANGLE/2) % TRIG_MAX_ANGLE;
+  dark_point.y = - dark_point.y;
+  return dark_point;
+}
+
 //piece of code modified from:
 //davidfg4/pebble-day-night
 void draw_earth(GBitmap* three_worlds, struct Layer* map_layer) {
@@ -10,28 +40,27 @@ void draw_earth(GBitmap* three_worlds, struct Layer* map_layer) {
 #else
   int now = (int)time(NULL);
 #endif
-  float day_of_year; // value from 0 to 1 of progress through a year
-  float time_of_day; // value from 0 to 1 of progress through a day
-  // approx number of leap years since epoch
-  // = now / SECONDS_IN_YEAR * .24; (.24 = average rate of leap years)
-  int leap_years = (int)((float)now / 131487192.0);
-  // day_of_year is an estimate, but should be correct to within one day
-  day_of_year = now - (((int)((float)now / 31556926.0) * 365 + leap_years) * 86400);
-  day_of_year = day_of_year / 86400.0;
-  time_of_day = day_of_year - (int)day_of_year;
-  day_of_year = day_of_year / 365.0;
-  // ##### calculate the position of the sun
-  // left to right of world goes from 0 to 65536
-  int sun_x = (int)((float)TRIG_MAX_ANGLE * (1.0 - time_of_day));
-  // bottom to top of world goes from -32768 to 32768
-  // 0.2164 is march 20, the 79th day of the year, the march equinox
-  // Earth's inclination is 23.4 degrees, so sun should vary 23.4/90=.26 up and down
-  int sun_y = -sin_lookup((day_of_year - 0.2164) * TRIG_MAX_ANGLE) * .26 * .25;
+  //get sun
+  struct my_point sun_point = get_sun_point(now);
+  int sun_x = sun_point.x;
+  int sun_y = sun_point.y;
+  //get dark
+  struct my_point dark_point = get_dark_point(now);
+  int32_t dark_x_map = dark_point.x * WIDTH / TRIG_MAX_ANGLE;
   // ##### draw the bitmap
   int x, y;
   for(x = 0; x < WIDTH; x++) {
     int x_angle = (int)((float)TRIG_MAX_ANGLE * (float)x / (float)(WIDTH));
     for(y = 0; y < HEIGHT; y++) {
+      //divider between day and night
+      if (dark_x_map == x && y%5 > 1){
+        #ifdef PBL_BW
+        #else
+        int byte_tmp = y * gbitmap_get_bytes_per_row(three_worlds) + x;
+        ((int8_t *)gbitmap_get_data(three_worlds))[byte_tmp] = -1;
+        continue;
+        #endif
+      }
       int y_angle = (int)((float)TRIG_MAX_ANGLE * (float)y / (float)(HEIGHT * 2)) - TRIG_MAX_ANGLE/4;
       // spherical law of cosines
       float angle = ((float)sin_lookup(sun_y)/(float)TRIG_MAX_RATIO) * ((float)sin_lookup(y_angle)/(float)TRIG_MAX_RATIO);
--- src/c/map_layer.h
+++ src/c/map_layer.h
@@ -6,5 +6,11 @@
 //const uint16_t 
 #define HEIGHT 72
 
+struct my_point{
+  int32_t x; //0 .. trig max angle
+  int32_t y; //-trig max angle/2 .. trig max angle/2
+};
+
+
 //void draw_map(struct Layer *layer, GContext *ctx);
 void draw_earth(GBitmap* three_worlds, struct Layer* map_layer);
